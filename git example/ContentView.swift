7964fcb369de2781030f6050ad279b72fecd189a
Alex <s014857@students.lmsd.org>
2021-03-11 10:59:10 -0500

    Initial Commit

---
+++ git example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  git example
+//
+//  Created by Alexander Bullard (student LM) on 3/11/21.
+//  Copyright © 2021 Alexander Bullard (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
